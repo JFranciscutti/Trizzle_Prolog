560ccf263229aacee3bd385f22ce4a87eba1f567
Cuttineta <54332299+Cuttineta@users.noreply.github.com>
2020-06-11 17:36:23 -0300

    Desplazamiento de columna implementado SATISFACTORIAMENTE

--- Juego.pl
+++ Juego.pl
@@ -5,21 +5,16 @@ obtenerT(X,2):- member(X,[v2,a2,r2]).
 obtenerT(X,3):- member(X,[v3,a3,r3]).
 
 
-obtenerFila(N,[L1,_L2,_L3,_L4,_L5],ListaD):-
-  N is 1,
-  ListaD = L1.
-obtenerFila(N,[_L1,L2,_L3,_L4,_L5],ListaD):-
-  N is 2,
-  ListaD = L2.
-obtenerFila(N,[_L1,_L2,L3,_L4,_L5],ListaD):-
-  N is 3,
-  ListaD = L3.
-obtenerFila(N,[_L1,_L2,_L3,L4,_L5],ListaD):-
-  N is 4,
-  ListaD = L4.
-obtenerFila(N,[_L1,_L2,_L3,_L4,L5],ListaD):-
-  N is 5,
-  ListaD = L5.
+obtenerFila(N,[L1,_L2,_L3,_L4,_L5],L1):-
+  N is 1.
+obtenerFila(N,[_L1,L2,_L3,_L4,_L5],L2):-
+  N is 2.
+obtenerFila(N,[_L1,_L2,L3,_L4,_L5],L3):-
+  N is 3.
+obtenerFila(N,[_L1,_L2,_L3,L4,_L5],L4):-
+  N is 4.
+obtenerFila(N,[_L1,_L2,_L3,_L4,L5],L5):-
+  N is 5.
 
 
 setFila(N,L,[_L1,L2,L3,L4,L5],[L,L2,L3,L4,L5]):-
@@ -32,6 +27,45 @@ setFila(N,L,[L1,L2,L3,_L4,L5],[L1,L2,L3,L,L5]):-
     N is 4.
 setFila(N,L,[L1,L2,L3,L4,_L5],[L1,L2,L3,L4,L]):-
     N is 5.
+
+obtenerColumna(Num,[L1,L2,L3,L4,L5],ListaN):-
+    obtenerElem(Num,L1,E1),
+    obtenerElem(Num,L2,E2),
+    obtenerElem(Num,L3,E3),
+    obtenerElem(Num,L4,E4),
+    obtenerElem(Num,L5,E5),
+    ListaN=[E1,E2,E3,E4,E5].
+
+setColumna(Num,[E1,E2,E3,E4,E5],[L1,L2,L3,L4,L5],[NL1,NL2,NL3,NL4,NL5]):-
+    setElem(E1,L1,Num,NL1),
+    setElem(E2,L2,Num,NL2),
+    setElem(E3,L3,Num,NL3),
+    setElem(E4,L4,Num,NL4),
+    setElem(E5,L5,Num,NL5).
+
+
+obtenerElem(Num,[E1,_E2,_E3,_E4,_E5],E1):-
+    Num is 1.
+obtenerElem(Num,[_E1,E2,_E3,_E4,_E5],E2):-
+    Num is 2.
+obtenerElem(Num,[_E1,_E2,E3,_E4,_E5],E3):-
+    Num is 3.
+obtenerElem(Num,[_E1,_E2,_E3,E4,_E5],E4):-
+    Num is 4.
+obtenerElem(Num,[_E1,_E2,_E3,_E4,E5],E5):-
+    Num is 5.
+
+setElem(Elem,[_E1,E2,E3,E4,E5],Pos,[Elem,E2,E3,E4,E5]):-
+    Pos is 1.
+setElem(Elem,[E1,_E2,E3,E4,E5],Pos,[E1,Elem,E3,E4,E5]):-
+    Pos is 2.
+setElem(Elem,[E1,E2,_E3,E4,E5],Pos,[E1,E2,Elem,E4,E5]):-
+    Pos is 3.
+setElem(Elem,[E1,E2,E3,_E4,E5],Pos,[E1,E2,E3,Elem,E5]):-
+    Pos is 4.
+setElem(Elem,[E1,E2,E3,E4,_E5],Pos,[E1,E2,E3,E4,Elem]):-
+    Pos is 5.
+
 
 generarTablero(Tablero):-
    generarFila(F1),
@@ -71,35 +105,43 @@ desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
   (Dir = 'der' ; Dir = 'izq'),
   obtenerFila(Num,Tablero,Lista),
   C is Cant mod 5, %verifico que el numero este entre 0 y 4 para no hacer movimientos redundantes
-  rotarFila(Dir,C,Lista,ListaN),
-  setFila(Num,ListaN,Tablero,TableroNuevo),
-  generarEvolTablero(TableroNuevo,EvolTablero).
+  rotar(Dir,C,Lista,ListaN),
+  setFila(Num,ListaN,Tablero,EvolTablero).
+  %generarEvolTablero(TableroNuevo,EvolTablero).
+
+desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
+    (Dir = 'abj' ; Dir = 'arb'),
+    obtenerColumna(Num,Tablero,Lista),
+    C is Cant mod 5,  %verifico que el numero este entre 0 y 4 para no hacer movimientos redundantes
+    rotar(Dir,C,Lista,ListaN),
+    setColumna(Num,ListaN,Tablero,EvolTablero).
+    %generarEvolTablero(TableroNuevo,EvolTablero).
 
 
-generarEvolTablero(Tablero,ListaTableros):-
+/*generarEvolTablero(Tablero,ListaTableros):-
   addLast(Tablero,[],L1),
   buscar_colapsos(Tablero,TColapsos),%genera colapsos si existen dsp de un desplazamiento
   addLast(TColapsos,L1,L2),
   agregar_x(TColapsos,TX), %llena de X los espacios en blanco.
   addLast(TX,L2,L3),
   randomPorX(TX,TFinal),%reemplaza las X por mamushkas random
-  addLast(TFinal,L3,ListaTableros).
+  addLast(TFinal,L3,ListaTableros).*/
 
 
 
-rotarFila(_Sentido,0,Lista,Lista):-!.
+rotar(_Sentido,0,Lista,Lista):-!.
 
-rotarFila(Sentido,Cant,Lista,ListaN):-
-  Sentido = 'der',
+rotar(Sentido,Cant,Lista,ListaN):-
+  (Sentido = 'der'; Sentido= 'abj'),
   shift_derecha(Lista,LN),
   C is Cant - 1,
-  rotarFila(Sentido,C,LN,ListaN).
+  rotar(Sentido,C,LN,ListaN).
 
-rotarFila(Sentido,Cant,Lista,ListaN):-
-  Sentido = 'izq',
+rotar(Sentido,Cant,Lista,ListaN):-
+  (Sentido = 'izq'; Sentido='arb'),
   shift_izquierda(Lista,LN),
   C is Cant - 1,
-  rotarFila(Sentido,C,LN,ListaN).
+  rotar(Sentido,C,LN,ListaN).
 
 
 /*PREDICADOS AUXILIARES REUTILIZADOS DE LOS PRACTICOS*/
@@ -117,24 +159,3 @@ rotarFila(Sentido,Cant,Lista,ListaN):-
 
   borrar(X,[X|T],T).
   borrar(X,[H|T],[H|R]):- borrar(X,T,R).
-
- /*    Idea columna
- movercolumna(Board, C , Cant, NBoard
-:-
-shift(Board, C, NBoard),
-Cant is Cant-1
-movercolumna
-  shiftAux(Board, Columna, Fila, NBoard)
-shift(Board, C, NBoard):-
-     shiftAux(Board, Columna, 0, NBoard).
-shift(Board, C, NBoard):-
-     getElemento(Board, Columna, 0, E),
-     shiftAux(Board, Columna, 0, E, NBoard).
-shiftAux(Board, Columna, Fila, Elemento, NBoard):-
-    Fila is Fila+1,
-    reemplazar(Board, Columna, Fila, Elemento, E1, Board1),
-    shiftAux(Board1, Columna, Fila, E1, NBoard)
-shiftAux(Board, Columna, 4, E, NBoard):-
-    reemplazar(Board, Columna, 0, _X, E).
-
-*/
