bf81b3fdbe62206c594cd981966437aaa96610ed
Cuttineta <54332299+Cuttineta@users.noreply.github.com>
2020-06-12 21:17:11 -0300

    cambio de posicion de las mamushkas que se upgradean

--- Juego.pl
+++ Juego.pl
@@ -121,15 +121,15 @@ desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
   (Dir = 'der' ; Dir = 'izq'),
   obtenerFila(Num,Tablero,Lista),
   rotar(Dir,Cant,Lista,ListaN),
-  setFila(Num,ListaN,Tablero,TableroNuevo),
-  generarEvolTablero(TableroNuevo,EvolTablero).
+  setFila(Num,ListaN,Tablero,EvolTablero).
+  %generarEvolTablero(TableroNuevo,EvolTablero).
 
 desplazar(Dir, Num, Cant, Tablero, EvolTablero):-
   (Dir = 'abj' ; Dir = 'arb'),
   obtenerColumna(Num,Tablero,Lista),
   rotar(Dir,Cant,Lista,ListaN),
-  setColumna(Num,ListaN,Tablero,TableroNuevo),
-  generarEvolTablero(TableroNuevo,EvolTablero).
+  setColumna(Num,ListaN,Tablero,EvolTablero).
+  %generarEvolTablero(TableroNuevo,EvolTablero).
 
 
 generarEvolTablero(Tablero,ListaTableros):-
@@ -181,17 +181,17 @@ buscar_en_columnas(Tablero,TableroN):-
 
   iguales_tres([E1,E2,E3,E4,E5],ListaN):-
       (E1=E2,E2=E3),
-      upgrade(E3,NE3),
-      ListaN=[x,x,NE3,E4,E5].
+      upgrade(E2,NE2),
+      ListaN=[x,NE2,x,E4,E5].
   iguales_tres([E1,E2,E3,E4,E5],ListaN):-
       (E2=E3,E3=E4),
-      upgrade(E4,NE4),
-      ListaN=[E1,x,x,NE4,E5].
+      upgrade(E3,NE3),
+      ListaN=[E1,x,NE3,x,E5].
 
   iguales_tres([E1,E2,E3,E4,E5],ListaN):-
       (E3=E4,E4=E5),
-      upgrade(E5,NE5),
-      ListaN=[E1,E2,x,x,NE5].
+      upgrade(E4,NE4),
+      ListaN=[E1,E2,x,NE4,x].
 
   iguales_cuatro([E1,E2,E3,E4,E5],ListaN):-
       (E1=E2,E2=E3,E3=E4),
